ed7d55fd31957f239e79b034100afc938db83e1a
Kano <serviri@gmail.com>
2019-11-27 11:03:37 +0100

    Implemented start and end x and y read

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include "route_planner.h"
 
 using namespace std::experimental;
+using namespace std;
 
 static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
 {   
@@ -51,16 +52,28 @@ int main(int argc, const char **argv)
         else
             osm_data = std::move(*data);
     }
-    
+
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+
+
+    float start_x, start_y = 10;
+    float end_x, end_y = 99;
+    cout << "Please enter the  start_x value" << endl;
+    cin >> start_x;
+    cout << "Please enter the  start_y value" << endl;
+    cin >> start_y;
+    cout << "Please enter the  end_x value" << endl;
+    cin >> end_x;
+    cout << "Please enter the  end_y value" << endl;
+    cin >> end_y;
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
